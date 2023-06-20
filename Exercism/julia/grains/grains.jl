61352e7aa5d9a1f73efd2c28533ecb73e041c0c6
Manuel <manuelalejandromartinezf@gmail.com>
2023-06-20 09:02:04 -0600

    g
    g

---
+++ Exercism/julia/grains/grains.jl
@@ -0,0 +1,12 @@
+"""Calculate the number of grains on square `square`."""
+function on_square(square)
+    65 > square > 0 || throw(DomainError(" "))
+    exp2(square-1)
+end
+
+"""Calculate the total number of grains after square `square`."""
+function total_after(square)
+    65 > square > 0 || throw(DomainError(" "))
+    square = Int128(square)
+    round(Int128, exp2(square)) - 1
+end
